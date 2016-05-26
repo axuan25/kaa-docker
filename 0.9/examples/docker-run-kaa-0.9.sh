c7d8867aeef5d9a83b3ea43ef9f20ee4549dee81
axuan25 <monroc@gmail.com>
2016-05-26 11:03:30 +0800

    update example run script

--- 0.9/examples/docker-run-kaa-0.9.sh
+++ 0.9/examples/docker-run-kaa-0.9.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 
+# Suggestion: create a docker network (say "kaa"), add all the required containers to this network, then run this script.
+
 docker run \
 -d \
 --name kaa \
---link zookeeper:zookeeper \
---link postgres:postgresql \
---link mongo:mongodb \
+--net=kaa \
 --env-file example-env.dockerenv \
 -p 8080:8080 \
 -p 9888:9888 \
 -p 9889:9889 \
 -p 9997:9997 \
--p 9999:9999 \
-xmight/kaa:0.8.1
+-p 9997:9997 \
+xmight/kaa:0.9.0
+
+# To view logs:
+# $ docker logs kaa
