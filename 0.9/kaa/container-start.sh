9aefa52a42fd6ca2269e7f3bc3c4172761ea1178
axuan25 <monroc@gmail.com>
2016-06-06 10:37:48 +0800

    fix container-start.sh

--- 0.9/kaa/container-start.sh
+++ 0.9/kaa/container-start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 . /kaa/configure-kaa.sh
-. /kaa/service-start.sh &
-sleep 5 && . /kaa/tail-node.sh
+. /kaa/service-start.sh &&
+. /kaa/tail-node.sh
 /bin/bash
 
 # sleep 5 before tail-node, or exits immediately
