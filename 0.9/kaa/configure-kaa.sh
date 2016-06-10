670ce2124135bf8646bc90c59ae916700c2f4a11
axuan25 <monroc@gmail.com>
2016-06-10 18:47:08 +0800

    - Use Ubuntu 16.04 - Make SQL DB configurable: SQL_PROVIDER_NAME=mariadb|postgres

--- 0.9/kaa/configure-kaa.sh
+++ 0.9/kaa/configure-kaa.sh
@@ -3,25 +3,39 @@
 ## Kaa Docker configurator
 ## - Christopher Burroughs @ xMight Inc. <chris@xmight.com>
 
-
-# Check if Zookeeper host + port are provided, then build ZK connection string.
-# If multiple ZK hosts exist, then only provide ZOOKEEPER_CONNECTION_STRING in the environment file.
-[ -n "$ZOOKEEPER_HOST" ] || ZOOKEEPER_HOST="localhost"
-[ -n "$ZOOKEEPER_PORT" ] || ZOOKEEPER_PORT="2181"
-[ -n "$ZOOKEEPER_CONNECTION_STRING" ] || ZOOKEEPER_CONNECTION_STRING="${ZOOKEEPER_HOST}:${ZOOKEEPER_PORT}"
-
-# Check if JDBC host:port + DB name are provided, then build JDBC URL
+# Check if JDBC host:port + DB name are provided, use defaults otherwise
 [ -n "$JDBC_HOST" ] || JDBC_HOST="localhost"
 [ -n "$JDBC_PORT" ] || JDBC_PORT="3306"
 [ -n "$JDBC_DB_NAME" ] || JDBC_DB_NAME="kaa"
-JDBC_URL="jdbc:mysql:failover://${JDBC_HOST}:${JDBC_PORT}/${JDBC_DB_NAME}"
 
-## TODO: use same technique for cassandra/mongo
+# Determine JDBC url and driver
+if [ $SQL_PROVIDER_NAME == "mariadb" ]
+then
+
+  $SQL_PROVIDER_NAME="mysql:failover"
+  $HIBERNATE_DIALECT="org.hibernate.dialect.MySQL5InnoDBDialect"
+  $JDBC_DRIVER_CLASSNAME="org.mariadb.jdbc.Driver"
+  $JDBC_URL="jdbc:mysql:failover://${JDBC_HOST}:${JDBC_PORT}/${JDBC_DB_NAME}"
+
+elif [ $SQL_PROVIDER_NAME == "postgresql" ]
+then
+
+  $SQL_PROVIDER_NAME="postgresql"
+  $HIBERNATE_DIALECT="org.hibernate.dialect.PostgreSQL82Dialect"
+  $JDBC_DRIVER_CLASSNAME="org.postgresql.Driver"
+  $JDBC_URL="jdbc:postgresql://${JDBC_HOST}:${JDBC_PORT}/${JDBC_DB_NAME}"
+
+else
+  echo "Incorrect SQL provider name: '${SQL_PROVIDER_NAME}'\nValid options: 'mariadb' , 'postgresql'\nConfiguration exiting now!"
+  exit 1
+fi
 
 ## Process configuration templates ##
 
 # > admin-dao.properties
 cat /usr/lib/kaa-node/conf/admin-dao.properties.template | sed \
+  -e "s|{{HIBERNATE_DIALECT}}|${HIBERNATE_DIALECT}|g" \
+  -e "s|{{JDBC_DRIVER_CLASSNAME}}|${JDBC_DRIVER_CLASSNAME}|g" \
   -e "s|{{JDBC_USERNAME}}|${JDBC_USERNAME:-sqladmin}|g" \
   -e "s|{{JDBC_PASSWORD}}|${JDBC_PASSWORD:-admin}|g" \
   -e "s|{{JDBC_URL}}|${JDBC_URL}|g" \
@@ -30,10 +44,13 @@ cat /usr/lib/kaa-node/conf/admin-dao.properties.template | sed \
 # > sql-dao.properties
 cat /usr/lib/kaa-node/conf/sql-dao.properties.template | sed \
   -e "s|{{JDBC_DB_NAME}}|${JDBC_DB_NAME}|g" \
+  -e "s|{{HIBERNATE_DIALECT}}|${HIBERNATE_DIALECT}|g" \
+  -e "s|{{JDBC_DRIVER_CLASSNAME}}|${JDBC_DRIVER_CLASSNAME}|g" \
   -e "s|{{JDBC_USERNAME}}|${JDBC_USERNAME:-sqladmin}|g" \
   -e "s|{{JDBC_PASSWORD}}|${JDBC_PASSWORD:-admin}|g" \
   -e "s|{{JDBC_HOST}}|${JDBC_HOST}|g" \
   -e "s|{{JDBC_PORT}}|${JDBC_PORT}|g" \
+  -e "s|{{SQL_PROVIDER_NAME}}|${SQL_PROVIDER_NAME}|g" \
    > /usr/lib/kaa-node/conf/sql-dao.properties
 
 # > mariadb-dao.properties
@@ -51,11 +68,11 @@ cat /usr/lib/kaa-node/conf/postgresql-dao.properties.template | sed \
    > /usr/lib/kaa-node/conf/postgresql-dao.properties
 
 # > common-dao-cassandra.properties
-CASSANDRA_NODE_LIST_DEFAULT="localhost:9042"
+[ -n "$CASSANDRA_NODE_LIST" ] || CASSANDRA_NODE_LIST="localhost:9042"
 cat /usr/lib/kaa-node/conf/common-dao-cassandra.properties.template | sed \
   -e "s|{{CASSANDRA_CLUSTER_NAME}}|${CASSANDRA_CLUSTER_NAME:-Kaa Cluster}|g" \
   -e "s|{{CASSANDRA_KEYSPACE_NAME}}|${CASSANDRA_KEYSPACE_NAME:-kaa}|g" \
-  -e "s|{{CASSANDRA_NODE_LIST}}|${CASSANDRA_NODE_LIST:-$CASSANDRA_NODE_LIST_DEFAULT}|g" \
+  -e "s|{{CASSANDRA_NODE_LIST}}|${CASSANDRA_NODE_LIST|g" \
   -e "s|{{CASSANDRA_USE_SSL}}|${CASSANDRA_USE_SSL:-false}|g" \
   -e "s|{{CASSANDRA_USE_JMX}}|${CASSANDRA_USE_JMX:-true}|g" \
   -e "s|{{CASSANDRA_USE_CREDENTIALS}}|${CASSANDRA_USE_CREDENTIALS:-false}|g" \
@@ -64,9 +81,9 @@ cat /usr/lib/kaa-node/conf/common-dao-cassandra.properties.template | sed \
    > /usr/lib/kaa-node/conf/common-dao-cassandra.properties
 
 # > common-dao-mongodb.properties
-MONGODB_NODE_LIST_DEFAULT="localhost:27017"
+[ -n "$MONGODB_NODE_LIST" ] || MONGODB_NODE_LIST="localhost:27017"
 cat /usr/lib/kaa-node/conf/common-dao-mongodb.properties.template | sed \
-  -e "s|{{MONGODB_NODE_LIST}}|${MONGODB_NODE_LIST:-$MONGODB_NODE_LIST_DEFAULT}|g" \
+  -e "s|{{MONGODB_NODE_LIST}}|${MONGODB_NODE_LIST}|g" \
   -e "s|{{MONGODB_DB_NAME}}|${MONGODB_DB_NAME:-kaa}|g" \
   -e "s|{{MONGODB_WRITE_CONCERN}}|${MONGODB_WRITE_CONCERN:-acknowledged}|g" \
    > /usr/lib/kaa-node/conf/common-dao-mongodb.properties
@@ -78,6 +95,7 @@ cat /usr/lib/kaa-node/conf/nosql-dao.properties.template | sed \
    > /usr/lib/kaa-node/conf/nosql-dao.properties
 
 # > kaa-node.properties
+[ -n "$ZOOKEEPER_NODE_LIST" ] || ZOOKEEPER_NODE_LIST="localhost:2181"
 cat /usr/lib/kaa-node/conf/kaa-node.properties.template | sed \
   -e "s|{{CONTROL_SERVER_ENABLED}}|${CONTROL_SERVER_ENABLED:-true}|g" \
   -e "s|{{BOOTSTRAP_SERVER_ENABLED}}|${BOOTSTRAP_SERVER_ENABLED:-true}|g" \
@@ -85,7 +103,7 @@ cat /usr/lib/kaa-node/conf/kaa-node.properties.template | sed \
   -e "s|{{THRIFT_HOST}}|${THRIFT_HOST:-localhost}|g" \
   -e "s|{{THRIFT_PORT}}|${THRIFT_PORT:-9090}|g" \
   -e "s|{{ADMIN_PORT}}|${ADMIN_PORT:-8080}|g" \
-  -e "s|{{ZOOKEEPER_CONNECTION_STRING}}|${ZOOKEEPER_CONNECTION_STRING}|g" \
+  -e "s|{{ZOOKEEPER_NODE_LIST}}|${ZOOKEEPER_NODE_LIST}|g" \
   -e "s|{{SUPPORT_UNENCRYPTED_CONNECTION}}|${SUPPORT_UNENCRYPTED_CONNECTION:-true}|g" \
   -e "s|{{TRANSPORT_BIND_INTERFACE}}|${TRANSPORT_BIND_INTERFACE:-0.0.0.0}|g" \
   -e "s|{{TRANSPORT_PUBLIC_INTERFACE}}|${TRANSPORT_PUBLIC_INTERFACE:-localhost}|g" \
--- 0.9/kaa/container-start.sh
+++ 0.9/kaa/container-start.sh
@@ -3,6 +3,7 @@
 touch /var/log/kaa/kaa-node.log;
 
 . /kaa/configure-kaa.sh &&
+echo "Kaa configured! Starting service..." &&
 service kaa-node start &
 . /kaa/tail-node.sh
 
